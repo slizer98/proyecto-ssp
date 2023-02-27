c9d7cb59521ef40c82fc6d83e707170c30feb33e
slizer98 <erick.m.villa.r@gmail.com>
2023-02-27 10:06:40 -0600

    agregando swagger para documentacion

--- index.js
+++ index.js
@@ -7,9 +7,40 @@ import homePage from './routes/homePageRouter.js'
 import capitulos from './routes/capitulosRouter.js';
 import proyectos from './routes/proyectosRouter.js';
 import actividades from './routes/actividadesRouter.js';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 import db from './config/db.js'; 
 
 const app = express();
+
+// swagger
+const swaggerOptions = {
+    swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'API asociación de mecatrónicos',
+        version: '1.0.0',
+        description: 'Documentación de la API con Swagger',
+        license: {
+          name: 'MIT',
+          url: 'https://opensource.org/licenses/MIT'
+        },
+        contact: {
+          name: 'Erick Villafuerte, Axel Ramos',
+          url: 'https://www.linkedin.com/in/erick-villafuerte/',
+          email: 'erick.m.villa.r@gmail.com'
+        }
+      },
+      servers: [
+        {
+          url: 'http://localhost:3000'
+        }
+      ]
+    },
+    apis: ['./routes/*.js']
+  };
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 app.use(cors());
 
@@ -26,13 +57,14 @@ try {
     console.log(error);
 }
 
-// rutas disponibles
+
 app.use('/auth', usuarioRoutes);
 app.use('/tareas', listaDeTareasRouter);
 app.use('/', homePage);
 app.use('/capitulos', capitulos);
 app.use('/proyectos', proyectos);
 app.use('/actividades', actividades);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 
 // ruta para rutas que no existen
--- routes/usuarioRouter.js
+++ routes/usuarioRouter.js
@@ -18,5 +18,40 @@ router.post('/olvide-password', olvidePassword);
 router.get('/recuperar-password/:token', recuperarPassword);
 router.get('/obtener-usuario/:id', obtenerUsuario);
 router.put('/editar-perfil/:id', editarUsuario);
+// documentacion
+// login
+/**
+ * @swagger
+ * /auth/login:
+  *   post:
+ *     summary: Autenticación de usuario
+ *     description: Autenticación de usuario utilizando ID y contraseña.
+ *     tags:
+ *       - Autenticación
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               AIMID:
+ *                 type: string
+ *                 example: "1676918003022"
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: "123456"
+ *             required:
+ *               - AIMID
+ *               - password
+ *     responses:
+ *       '200':
+ *         description: Usuario autenticado correctamente
+ *       '401':
+ *         description: Credenciales inválidas
+ *       '500':
+ *         description: Error interno del servidor
+ */
 
 export default router;
